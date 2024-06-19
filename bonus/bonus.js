e53e2dee965354018ced78b72a2d32ce867892b0
TommasoGalistu <tommasogalistu92@gmail.com>
2024-06-19 16:17:28 +0200

    func: start, stop and reverse complete

--- bonus/bonus.js
+++ bonus/bonus.js
@@ -5,34 +5,35 @@ let reverseButton = document.getElementById('reverse');
 
 let clicked = false;
 let startTime = setInterval(avanti, 3000);
+let startEvent;
 let reverseTime;
 if(!clicked){
     startTime
 }
 
 stopButton.addEventListener('click', () => {
-    if(reverseTime){
-        clearInterval(reverseTime);
-        reverseButton = undefined;
-    }else{
-        clearInterval(startTime);
-    }
-    
+    clearInterval(startTime);
+    clearInterval(reverseTime);
+    clearInterval(startEvent);
 
 });
 
 reverseButton.addEventListener('click', () =>{
     clearInterval(startTime);
+    clearInterval(reverseTime);
+    clearInterval(startEvent)
     reverseTime = setInterval(indietro, 3000);
 })
 
-// start.addEventListener('click', () =>{
-    
-//     if(!clicked){
-//         clearInterval(startTime);
-//         startTime
-//         clicked = true;
-//     }
-// });
+start.addEventListener('click', () =>{
+    if(startEvent){
+        clearInterval(startEvent)
+        console.log('piu di un click')
+        console.log(startEvent)
+    }
+    clearInterval(startTime);
+    clearInterval(reverseTime);
+    startEvent = setInterval(avanti, 3000);
+});
 
 
