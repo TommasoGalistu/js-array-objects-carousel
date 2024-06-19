cf0b91c0727ae4e7f47d9065029c81fb5d223ddd
TommasoGalistu <tommasogalistu92@gmail.com>
2024-06-19 15:58:36 +0200

    add: stop button

--- bonus/bonus.js
+++ bonus/bonus.js
@@ -1,1 +1,25 @@
-setInterval(avanti, 3000);
+let startButton = document.getElementById('start');
+let stopButton = document.getElementById('stop');
+let reverseButton = document.getElementById('reverse');
+
+
+let clicked = false;
+let startTime = setInterval(avanti, 2000);
+if(!clicked){
+    startTime
+}
+
+stopButton.addEventListener('click', () => {
+    clearInterval(startTime);
+});
+
+// start.addEventListener('click', () =>{
+    
+//     if(!clicked){
+//         clearInterval(startTime);
+//         startTime
+//         clicked = true;
+//     }
+// });
+
+
