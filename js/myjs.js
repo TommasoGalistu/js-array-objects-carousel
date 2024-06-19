6667bf9f7028cc00fc4d31c9a08e074fa78ce181
TommasoGalistu <tommasogalistu92@gmail.com>
2024-06-19 14:14:19 +0200

    func: add photo in page with js

--- js/myjs.js
+++ js/myjs.js
@@ -6,6 +6,10 @@ let contFotoPiccole = document.querySelector('.contFotoPiccole');
 const bottIndietro = document.querySelector('.buttonIndietro');
 const bottAvanti = document.querySelector('.buttonAvanti');
 
+// variabili testo da inserire
+let titoloFoto = document.getElementById('titleSuper');
+let testoFoto = document.getElementById('testoSuper');
+
 
 // creazione array con indirizzi foto
 let arrFoto = [
@@ -15,6 +19,9 @@ let arrFoto = [
     'img/04.webp',
     'img/05.webp',
 ];
+
+
+
 // creazione di array descrizione ALT foto
 let arrDescrizione = [
     'foto spiderman',
@@ -24,29 +31,83 @@ let arrDescrizione = [
     'immagine avengers',
 ];
 
+const images = [
+    {
+        'image': 'img/01.webp',
+        'title': "Marvel's Spiderman Miles Morales",
+        'text': 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+        'descrizione': 'foto di Marvel\'s Spiderman Miles Morales',
+    },
+    {
+        'image': 'img/02.webp',
+        'title': 'Ratchet & Clank: Rift Apart',
+        'text': 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+        'descrizione': 'foto di Ratchet & Clank: Rift Apart',
+    },
+    {
+        'image': 'img/03.webp',
+        'title': 'Fortnite',
+        'text': 'Grab all of your friends and drop into Epic Games Fortnite, a massive 100-player face-off that combines looting, crafting, shootouts and chaos.',
+        'descrizione': 'foto di Fortnite',
+    },
+    {
+        'image': 'img/04.webp',
+        'title': 'Stray',
+        'text': 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city.',
+        'descrizione': 'foto di Stray',
+    },
+    {
+        'image': 'img/05.webp',
+        'title': "Marvel's Avengers",
+        'text': "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
+        'descrizione': 'foto di Marvel\'s Avengers',
+    },
+];
 // individuare il numero di foto per fagli cambiare dimensione 
 let numFoto = 0;
 
 // ciclo per creare immagini da database e inserirle
-for (let i = 0; i < arrFoto.length; i++) {
-    // creazione di due nodi
-    let itemFoto = document.createElement('img');
-    let itemFoto2 = document.createElement('img');
-    // modifica src
-    itemFoto.src = arrFoto[i];
-    itemFoto2.src = itemFoto.src;
-    // modifica alt
-    itemFoto.alt = arrDescrizione[i];
-    itemFoto2.alt = itemFoto.alt;
-    // aggiunta della classe per gestire l'apparizione
-    itemFoto.classList.add('fotoPrincipale');
-    itemFoto2.classList.add('fotoPrincipale2');
-    // appendo i due nodi ai container
-    contFotoPrincipale.append(itemFoto);
-    contFotoPiccole.append(itemFoto2);
-    // contatore foto così da gestire le dimensioni, guarda sotto il for
-    numFoto++;
-}
+// for (let i = 0; i < arrFoto.length; i++) {
+//     // creazione di due nodi
+//     let itemFoto = document.createElement('img');
+//     let itemFoto2 = document.createElement('img');
+//     // modifica src
+//     itemFoto.src = arrFoto[i];
+//     itemFoto2.src = itemFoto.src;
+//     // modifica alt
+//     itemFoto.alt = arrDescrizione[i];
+//     itemFoto2.alt = itemFoto.alt;
+//     // aggiunta della classe per gestire l'apparizione
+//     itemFoto.classList.add('fotoPrincipale');
+//     itemFoto2.classList.add('fotoPrincipale2');
+//     // appendo i due nodi ai container
+//     contFotoPrincipale.append(itemFoto);
+//     contFotoPiccole.append(itemFoto2);
+//     // contatore foto così da gestire le dimensioni, guarda sotto il for
+//     numFoto++;
+// }
+
+// nuovo ciclo nel nuovo array di oggetti
+images.forEach(superEroe =>{
+    // creo le due immagine tag
+    let card = document.createElement('img');
+    let card2 = document.createElement('img');
+    // le caratterizzo con classi immagine ecc
+    card.classList.add("fotoPrincipale");
+    card2.classList.add("fotoPrincipale2");
+    card.src = superEroe.image;
+    card2.src = superEroe.image;
+    card.alt = superEroe.descrizione;
+    card2.alt = superEroe.descrizione;
+    // aggiungo la descrizione
+    titoloFoto.textContent = superEroe.title;
+    testoFoto.textContent = superEroe.text;
+    // lo aggiungo al contenitore
+    contFotoPrincipale.append(card);
+    contFotoPiccole.append(card2)
+    numFoto++
+    
+})
 
 // creazione dinamica della dimensione foto laterale
 let modificaNumFoto = document.documentElement.style.setProperty('--numFoto', numFoto);
